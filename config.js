226f6b81950d0418da6d0e7f2585629dd244b20a
ChinQuan <kamilmobile16@gmail.com>
2025-07-13 12:30:15 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// === config.js ===
+export const BACKEND_URL = 'https://bull-backend-71rj.onrender.com';
+export const BULL_TOKEN_MINT = 'BuLL65dUKeRgZ1TUo3g9F3SAgJmdwq23mcx7erb9QX9D';
+export const GAME_WALLET = '3UKEyGjUJadQqQaBwaiEtD5zNZEh7ZgCyDRnf6Ao2oEA'; // np. ten, na który będą trafiać tokeny
